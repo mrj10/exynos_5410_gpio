36987bd47823c6c696973cfb8a197ba9cfc7f02d
Matt Johnson <johnso87@illinois.edu>
2013-10-21 23:09:10 -0500

    Toggle a pin a bunch of times in the test program to get timing info

--- odroid_xu_test.c
+++ odroid_xu_test.c
@@ -13,49 +13,31 @@
 #define ENABLE_PULLUP 0x1 // PU resistor
 #define ENABLE_PULLDOWN 0x2 // PD resistor
 
+void print_all_gpio_info() {
+	printf("0x0C20 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_CON_REG_OFFSET));
+	printf("0x0C20 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_DATA_REG_OFFSET));
+	printf("0x0C20 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_UPD_REG_OFFSET));
+	printf("0x0C20 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
+	printf("0x0C40 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_CON_REG_OFFSET));
+	printf("0x0C40 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_DATA_REG_OFFSET));
+	printf("0x0C40 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_UPD_REG_OFFSET));
+	printf("0x0C40 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
+	printf("0x0C60 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_CON_REG_OFFSET));
+	printf("0x0C60 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_DATA_REG_OFFSET));
+	printf("0x0C60 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_UPD_REG_OFFSET));
+	printf("0x0C60 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
+	for(int i = 13; i <= 27; i++) {
+		printf("Pin %d = %u\n", i, odroid_xu_gpio_read(i));
+	}
+}
+
 int main(int argc, char *argv[]) {
 	exynos_5410_gpio_init();
-	printf("0x0C20 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_CON_REG_OFFSET));
-	printf("0x0C20 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_DATA_REG_OFFSET));
-	printf("0x0C20 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_UPD_REG_OFFSET));
-	printf("0x0C20 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
-	printf("0x0C40 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_CON_REG_OFFSET));
-	printf("0x0C40 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_DATA_REG_OFFSET));
-	printf("0x0C40 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_UPD_REG_OFFSET));
-	printf("0x0C40 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
-	printf("0x0C60 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_CON_REG_OFFSET));
-	printf("0x0C60 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_DATA_REG_OFFSET));
-	printf("0x0C60 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_UPD_REG_OFFSET));
-	printf("0x0C60 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
-	for(int i = 13; i <= 27; i++) {
-		printf("Pin %d = %u\n", i, odroid_xu_gpio_read(i));
-	}
-
+	//print_all_gpio_info();
 	odroid_xu_gpio_setup_all_output();
 
-	printf("0x0C20 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_CON_REG_OFFSET));
-	printf("0x0C20 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_DATA_REG_OFFSET));
-	printf("0x0C20 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_UPD_REG_OFFSET));
-	printf("0x0C20 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C20 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
-	printf("0x0C40 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_CON_REG_OFFSET));
-	printf("0x0C40 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_DATA_REG_OFFSET));
-	printf("0x0C40 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_UPD_REG_OFFSET));
-	printf("0x0C40 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C40 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
-	printf("0x0C60 bank CON register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_CON_REG_OFFSET));
-	printf("0x0C60 bank DATA register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_DATA_REG_OFFSET));
-	printf("0x0C60 bank UPD register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_UPD_REG_OFFSET));
-	printf("0x0C60 bank DRIVE_STRENGTH register = 0x%08X\n", exynos_5410_gpio_read_raw_reg(0x0C60 + EXYNOS_GPIO_DRIVE_STRENGTH_REG_OFFSET));
-
-	for(int i = 13; i <= 27; i++) {
-		printf("Pin %d = %u\n", i, odroid_xu_gpio_read(i));
-	}
-
-	odroid_xu_gpio_toggle(27);
-
-	printf("Toggled pin 27\n");
-	for(int i = 13; i <= 27; i++) {
-		printf("Pin %d = %u\n", i, odroid_xu_gpio_read(i));
-	}
+	for(int i = 0; i < 10000000; i++)
+		odroid_xu_gpio_toggle(27);
 
 	exynos_5410_gpio_destroy();
 	return 0;
